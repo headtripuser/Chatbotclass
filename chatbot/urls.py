d5e4f769e41d7cf71a22d44dc401888dc8ede023
Matzecoding <matthias.philipp92@gmail.com>
2025-01-31 14:43:11 +0100

    neueste django version ohne streaming

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,23 +1,14 @@
-"""
-URL configuration for chatbot project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # chatbot/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+# Funktion für eine einfache Startseite
+def home(request):
+    return HttpResponse("<h1>Willkommen beim Chatbot!</h1>")
 
 urlpatterns = [
-    path('', include('headtripbot.urls')),
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),  # Startseite hinzufügen
+    path('chat/', include('headtripbot.urls')),
 ]
