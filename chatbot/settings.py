91acec18e033ddeb397219fe16bf4fe2a853b7a0
Matzecoding <matthias.philipp92@gmail.com>
2025-01-31 14:34:36 +0100

    neueste django version ohne streaming

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'DenSecretKeyErrätKeinerhaha!$23'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "chatbotclass.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "chatbotclass.onrender.com", "chatbot.onrender.com"]
 
 
 
